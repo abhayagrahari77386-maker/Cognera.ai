bcc37353e1d2f310fd18c84dcd4fa2f91882ec44
abhayagrahari77386-maker <abhayagrahari77386@gmail.com>
2026-04-18 01:34:22 +0530

    Fixed Vercel ESM Compilation Crash

--- api/[...cognera].js
+++ api/[...cognera].js
@@ -1,8 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const path = require('path');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, '.env') });
 
 console.log('OPENROUTER_API_KEY loaded:', process.env.OPENROUTER_API_KEY ? `${process.env.OPENROUTER_API_KEY.substring(0, 10)}...` : 'MISSING');
@@ -945,4 +948,4 @@ if (process.env.NODE_ENV !== "production") {
   });
 }
 
-module.exports = app;
+export default app;
